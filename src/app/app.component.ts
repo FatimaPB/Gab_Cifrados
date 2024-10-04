74524a024025a3563d2631d2a0381ab5b3483637
GABBRIELJUA <20221040@uthh.edu.mx>
2024-10-03 21:53:33 -0600

    seg comi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive,
+  MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'cifrados';
+  isMenuOpen = false; // Estado del menú
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Cambia el estado al hacer clic
+  }
+  
+
+  
 }
